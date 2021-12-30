b3de1eecbb9f93c371a40a867bb95b7266dd0f67
Farok-Hossain <farokafs7@gmail.comm>
2021-12-31 01:13:28 +0600

    another files added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBMkewUMLDUpD_5e96sZuXKnnmXQUZg6VU",
+    authDomain: "ema-john-simple-aec32.firebaseapp.com",
+    projectId: "ema-john-simple-aec32",
+    storageBucket: "ema-john-simple-aec32.appspot.com",
+    messagingSenderId: "441219429902",
+    appId: "1:441219429902:web:6db0e0230dc1e7ee122e5f"
+  };
+
+  export default firebaseConfig;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+
+const initializeAuthentication = () =>{
+    initializeApp(firebaseConfig);
+}
+
+export default initializeAuthentication;
+
+/*
+steps for authentication 
+--------------------
+Step-1: Initial setup 
+1. firebasse: create project
+2. create web app
+3. get configuration
+4. initialize firebase
+5. Enable auth method
+
+------------
+Step-2: setup component
+1. Create Login Component
+2. Create Register Component
+3. Create Route for login and register
+
+
+------------------
+Step 3: set auth system
+1. set up sign in method
+2. setup sign out method
+3. user state
+4. special observe
+5. return necessary methods and states from useFirebase
+---------------
+ Step 4: create auth context hook (useAuth)
+ 1. create a auth context
+ 2. create context provider 
+ 3. set context Provider context value
+ 4. use auth provider
+ 5. create useAuth Hook
+---------------
+Step 5: create private route
+1. create private Route
+2. set private route
+---------------
+step-6: Redirect after login
+1. after login redirect user to their desired destination
+*/
