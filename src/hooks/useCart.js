2ab45f66b5cf5e2338b8797bc07a593ff4bfe34e
Farok-Hossain <farokafs7@gmail.comm>
2022-02-06 00:16:57 +0600

    another code added

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from 'react';
 import { getStoredCart } from '../utilities/fakedb';
 
-const useCart = products => {
+const useCart = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        const savedCart = getStoredCart();
+        const keys = Object.keys(savedCart);
+        fetch('http://localhost:5000/products/byKeys', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(keys)
+        })
+        .then(res => res.json())
+        .then(products => {
 
-        if (products.length) {
-            const savedCart = getStoredCart();
+            if (products.length) {
             const storedCart = [];
             for (const key in savedCart) {
                 const addedProduct = products.find(product => product.key === key);
@@ -21,7 +31,11 @@ const useCart = products => {
             setCart(storedCart);
         }
 
-    }, [products]);
+        })
+
+        
+
+    }, []);
 
     return [cart, setCart];
 }
